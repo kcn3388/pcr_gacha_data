fb4e4677b4c0e9f741ed3ff9457e5587109a7dd9
kcn3388 <kcn3388@126.com>
2023-04-21 20:17:15 +0800

    add mikan url

--- get_data.py
+++ get_data.py
@@ -48,28 +48,32 @@ def get_data(url: Union[List[str], str], path: Union[List[str], str]):
 
 
 parser = argparse.ArgumentParser(description="select which file to download")
-parser.add_argument("--every", "-E", action="store_true", help="download all json")
+parser.add_argument("--all", "-A", action="store_true", help="download all json")
 parser.add_argument("--ver", "-V", action="store_true", help="download version json")
 parser.add_argument("--unit", "-U", action="store_true", help="download unit_data json")
 parser.add_argument("--gacha", "-G", action="store_true", help="download gacha json")
+parser.add_argument("--mikan", "-M", action="store_true", help="download mikan rss")
 
 if __name__ == "__main__":
     ver_url = 'https://api.redive.lolikon.icu/gacha/gacha_ver.json'
     gacha_url = 'https://api.redive.lolikon.icu/gacha/default_gacha.json'
     unit_data_url = 'https://api.redive.lolikon.icu/gacha/unitdata.py'
+    mikan_url = "https://mikanani.me/RSS/MyBangumi?token=qZW4pvF0J6U1CJYNVk5uBw=="
 
     ver_path = os.path.join(os.path.dirname(__file__), 'gacha_ver.json')
     gacha_path = os.path.join(os.path.dirname(__file__), 'default_gacha.json')
     unit_data_path = os.path.join(os.path.dirname(__file__), 'unitdata.py')
+    mikan_path = os.path.join(os.path.dirname(__file__), 'mikan.xml')
 
     url_ = [ver_url, gacha_url, unit_data_url]
     path_ = [ver_path, gacha_path, unit_data_path]
 
     args = vars(parser.parse_args())
-    every = args["every"]
+    every = args["all"]
     ver = args["ver"]
     unit = args["unit"]
     gacha = args["gacha"]
+    mikan = args["mikan"]
     if every:
         get_data(url_, path_)
     elif ver:
@@ -78,5 +82,7 @@ if __name__ == "__main__":
         get_data(unit_data_url, unit_data_path)
     elif gacha:
         get_data(gacha_url, gacha_path)
+    elif mikan:
+        get_data(mikan_url, mikan_path)
     else:
         print("not enough args")
